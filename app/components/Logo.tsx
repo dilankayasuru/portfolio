9774cdc621aa867f695457ade53c30e6f87a0f84
dilankayasuru <dilankayasuru001@gmail.com>
2025-04-12 23:37:46 +0530

    change logo location

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react";
 export const Logo = () => {
     return (
         <motion.div
-            className="fixed top-0 left-0 z-50 p-6 mix-blend-multiply"
+            className="absolute top-0 left-0 z-50 p-6 mix-blend-multiply"
             transition={{ type: "easeInOut", delay: 1.7, duration: 0.5 }}
             initial={{ x: -100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Hero } from "./sections/Hero";
-import { Logo } from "./components/Logo";
 import { About } from "./sections/About";
 import { Contact } from "./sections/Contact";
 import { Work } from "./sections/Work";
@@ -8,7 +7,6 @@ import { Footer } from "./components/Footer";
 export default function Home() {
   return (
     <>
-      <Logo />
       <Hero />
       <Work />
       <About />
